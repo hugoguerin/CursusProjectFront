f510186c2c41ab569f74afed760e9ac16e7b34ad
hugoguerin <guerin.hugo.95@gmail.com>
2023-05-20 09:55:24 +0200

    ajout fonction tp

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function canMove (array, position) {
 }
 
 let playerPosition = getPlayerPosition(board, 3);
+
 function getPlayerPosition(array, number) {
     // parcourir le tableau puis return la position du joueur quand trouvé?
     for (let x = 0; x < array.length; x++) {
@@ -28,33 +29,41 @@ function getPlayerPosition(array, number) {
     }
 }
 
+const boardHtml = document.querySelector("#board");
 
 function teleport(array, playerPosition, desiredPosition ) {
-// si la case est vide -> le joueur prend la position sinon ciao bye
-    let valueOfdesiredPosition = array[desiredPosition.x][desiredPosition.y];
-    canMove(array, desiredPosition)
-    let holdPosition = playerPosition;
-    playerPosition = desiredPosition;
- 
+// si la case est vide -> le joueur prend la position sinon ciao bye;
+    if (canMove(array, desiredPosition)) { 
+        array[playerPosition.x][playerPosition.y] = 0;
+        array[desiredPosition.x][desiredPosition.y] = 3;  
+        createBoard();
+    }    
+}
 
 
+// const boardHtml = document.getElementById("board");
+function createBoard() {
+    boardHtml.replaceChildren();
+    
+    const body = document.createElement("tbody");
+    for (let x = 0; x < board.length; x++) {
+        const tr = document.createElement("tr");
+        for (let y = 0; y < board[x].length; y++) {
+            const td = document.createElement("td");
+            td.innerHTML = board[x][y];
+            // td.dataset.x = x;
+            // td.dataset.y = y;
+            td.addEventListener("click", function (event) {
+                // console.log(event.currentTarget);
+                teleport(board, getPlayerPosition(board, 3), { x: x, y: y });
+            });
+            tr.appendChild(td);
+        }
+        body.appendChild(tr);
+    }
 
+    boardHtml.appendChild(body);
 
 }
-canMove(board, 4, 2);
-getPlayerPosition(board, 3);
-console.log(board[0]);
-console.log(board[1]);
-console.log(board[2]);
-console.log(board[3]);
-console.log(board[4]);
-let desiredPosition = { x: 0, y: 0 };
-teleport(board, playerPosition, desiredPosition);
-console.log(board[0]);
-console.log(board[1]);
-console.log(board[2]);
-console.log(board[3]);
-console.log(board[4]);
 
-console.log(playerPosition);
-
+createBoard();

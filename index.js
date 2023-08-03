7d164ba24354eb23f6c871d2ffb344c6b909a856
Hugo Guerin <guerin.hugo.95@gmail.com>
2023-08-03 23:25:47 +0200

    new pc

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const board = [
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 0, 2, 2, 0, 0, 2, 2, 0, 2],
-    [2, 0, 2, 0, 0, 0, 0, 2, 0, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 0, 0, 0, 3, 0, 0, 0, 0, 2],
-    [2, 0, 2, 0, 0, 0, 0, 2, 0, 2],
-    [2, 0, 2, 2, 0, 0, 2, 2, 0, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    [2,2,2,2,2,2,2,2,2],
+    [2,3,0,0,0,0,0,1,2],
+    [2,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,1,2],
+    [2,0,0,0,0,0,1,4,2],
+    [2,2,2,2,2,2,2,2,2],
+
 ];
 
-// 0= case vide -- 1 = case occupée par un adversaire -- 2 = mur -- 3= joueur
+// 0= empty -- 1 = enemy -- 2 = wall -- 3= player -- 4 = goal
 
 
 
 function canMove (array, position) {
     const selected = array[position.x][position.y];
-    return selected == 0;
+    return selected == 0 || selected == 4;
 }
 
 let playerPosition = getPlayerPosition(board, 3);
@@ -57,17 +54,27 @@ function createBoard() {
             const td = document.createElement("td");
             // td.innerHTML = board[x][y]; 
             if (board[x][y] == 0) {
-                td.style.backgroundColor = "antiquewhite";
+                // td.style.backgroundColor = "antiquewhite"; <-------------- changement du fond en js direct
+                td.classList.add("empty");
             }  
             if (board[x][y] == 1) {
-                td.style.backgroundColor = "red";
+                // td.style.backgroundColor = "red";
+                td.classList.add("enemy");
             }  
             if (board[x][y] == 2) {
-                td.style.backgroundColor = "maroon";
+                // td.style.backgroundColor = "maroon";
+                td.classList.add("wall");
             }  
             if (board[x][y] == 3) {
-                td.style.backgroundColor = "green";
+                // td.style.backgroundColor = "green";
+                td.classList.add("player");
+
             }  
+            if (board[x][y] == 4) {
+                // td.style.backgroundColor = "lightblue";
+                td.classList.add("goal");
+            }  
+            
 
             // td.dataset.x = x;
             // td.dataset.y = y;
@@ -85,3 +92,19 @@ function createBoard() {
 }
 
 createBoard();
+
+
+// let isPlaying = true;
+// while (isPlaying) {
+//     isPlaying = false;
+//     document.getElementById("board").classList.add("hidden")
+//     for (let x = 0; x < board.length; x++) {
+//         for (let y = 0; y < board[x].length; y++) {
+//             if (board[x][y] == 4) {
+//                 isPlaying = true;
+//                 document.getElementById("board").classList.add("hidden")
+//             }        
+//         }
+//     }
+    
+// }

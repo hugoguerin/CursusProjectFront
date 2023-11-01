0d1c3e7acd50ad8cc491385de5d6a06cb9399872
Hugo <guerin.hugo.95@gmail.com>
2023-11-01 16:38:18 +0100

    feat: add isAligned and canTarget

--- src/spells/push.js
+++ src/spells/push.js
@@ -4,22 +4,8 @@ import { Entity, Type } from "../constants/index.js";
 //TODO RANGE OF SPELL USING action.range && ALLOW TO PUSH ON GOAL TILE? && ADD KEY IN DB FOR THE PUSH DISTANCE
 export function pushEnemy(board, playerPos, playerHtml, targetPos, action) {
 
-    const enemyPos = board[targetPos.x][targetPos.y];
-
-    if (enemyPos.entity != Entity.Enemy) {
-        console.log("Not an enemy");
-        return;
-    }
-
     const isAlignedX = playerPos.x == targetPos.x;
     const isAlignedY = playerPos.y == targetPos.y;
-    const isAligned = isAlignedX || isAlignedY;
-
-    if (!isAligned) {
-        console.log("Not aligned");
-        return;
-    }
-
     const directionX = playerPos.x - targetPos.x;
     const directionY = playerPos.y - targetPos.y;
     
@@ -33,7 +19,7 @@ export function pushEnemy(board, playerPos, playerHtml, targetPos, action) {
            behindEnemy = board[targetPos.x][targetPos.y - 1];
            console.log("Push to the left");
         }
-    } else {
+    } else if (isAlignedY) {
         if (directionX < 0) {
             behindEnemy = board[targetPos.x + 1][targetPos.y];     
             console.log("Push to the bottom");
@@ -47,6 +33,7 @@ export function pushEnemy(board, playerPos, playerHtml, targetPos, action) {
         behindEnemy.type != Type.Goal &&
         behindEnemy.type != Type.Wall
     ) {
+        const enemyPos = board[targetPos.x][targetPos.y];
         enemyPos.entity = Entity.None;
         behindEnemy.entity = Entity.Enemy;
         updateBoard(board, playerHtml);
